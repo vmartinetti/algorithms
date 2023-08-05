67818ecb50cb3c46746f7273ce5e7cb01451c3ee
Victor Martinetti <victor.martinetti@digipar.com>
2023-08-05 13:18:28 -0400

    Ineficient and eficient working solutions

--- largest-rectangle-histogram/index.js
+++ largest-rectangle-histogram/index.js
@@ -4,10 +4,15 @@ var largestRectangleArea = function(heights) {
   for(let i = 0; i < heights.length; i++) {
     let currentHeight = heights[i];
     let currentWidth = 1;
-    for(let ii=0; ii < heights.length; ii++) {
-      if(heights[ii] <= currentHeight) {
-        currentWidth ++
-      }
+    let ii = i+1;
+    let iii = i-1;
+    while(ii <= heights.length && heights[ii] >= currentHeight) {
+      currentWidth ++;
+      ii++;
+    }
+    while(iii >= 0 && heights[iii] >= currentHeight) {
+      currentWidth ++;
+      iii--;
     }
     let currentArea = currentHeight * currentWidth;
     if(currentArea > maxArea) {
@@ -16,3 +21,19 @@ var largestRectangleArea = function(heights) {
   }
   return maxArea;
 };
+// TODO: Understand this solution
+var largestRectangleArea2 = function(heights) {
+  let stack = [];
+  let maxArea = 0;
+  for(let i = 0; i <= heights.length; i++) {
+    let height = (i === heights.length ? 0 : heights[i]);
+    while(stack.length > 0 && heights[stack[stack.length - 1]] >= height) {
+      let j = stack.pop();
+      let width = (stack.length === 0 ? i : i - stack[stack.length - 1] - 1);
+      maxArea = Math.max(maxArea, heights[j] * width);
+    }
+    stack.push(i);
+  }
+  return maxArea;
+};
+module.exports = largestRectangleArea;
